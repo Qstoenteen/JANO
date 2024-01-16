eb9a49fcd3beb12d8c54e1746cbaa8be65bb6b23
Meleshkov_alex <dienteen@gmail.com>
2024-01-17 00:15:21 +0300

    upd

---
+++ TempClasses.py
@@ -0,0 +1,72 @@
+import pygame, math, random as r, sys,time
+import input_data
+print(input_data.config)
+pygame.init()
+clock = pygame.time.Clock()
+WIDTH = 1280
+HEIGHT = 720
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Pygame MyGame')
+icon = pygame.image.load('assets/character_default.png')
+FPS = 60
+running = True
+myfont = pygame.font.Font(None,25)
+text_surface = myfont.render('Forest biome',False,(255,255,255))
+
+GexInit = False
+class Gex ():
+    global GexInit
+    def __init__(self,biome,x,y,width,height):
+        self.biome = biome
+        self.x = x
+        self.y = y 
+        self.width = width
+        self.height = height
+    def draw(self):
+        pygame.draw.rect(sc,(255,255,255),(50,50,self.width,self.height),1)
+    def point(self):
+        pygame.draw.circle(sc,(255,255,255),(self.x + self.width//2, self.y + self.height//2), 50, 1)
+    
+            
+
+
+
+class Player ():
+    global position
+    def __init__(self, x, y, position, hp, damage, level, counter = int):
+        self.x = x
+        self.y = y
+        self.position = position
+        self.hp = hp
+        self.damage = damage
+        self.level = level
+        self.counter = counter
+    def draw(self):
+        if self.counter == 9:
+            self.counter = -1
+        self.counter += 1
+        
+forest = Gex('forest',50,50,400,400)    
+    
+    
+    
+def gex_init():
+    forest.draw()
+    
+    
+    
+while running:
+
+    sc.fill((25,25,50))
+    
+    
+    
+    forest.draw()
+    forest.point()
+    pygame.display.update()
+    for event in pygame.event.get():
+       
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    clock.tick(FPS)
